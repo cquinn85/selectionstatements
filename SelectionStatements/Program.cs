c29fcc0d2379c54b5b14d7cc945f6dcb8bbdbaff
cquinn85 <cequinn300@gmail.com>
2020-10-27 17:41:03 -0500

    Selection statements complete

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+
+            Console.WriteLine("Input your guess:");
+            var r = new Random();
+            var number = r.Next(1, 10);
+     
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your guess is to high!");
+            }
+            else if(guess < number)
+            {
+                Console.WriteLine("Your guess is to low!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind!");
+            }
+             
+
+            
+        }
+    }
+}
